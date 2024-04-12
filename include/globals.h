aecf12d418225ada76dba853f58441daa0b96cde
Caleb Stein <caleb@calebstein.net>
2024-04-12 16:53:07 -0700

    add global program pointers

--- include/globals.h
+++ include/globals.h
@@ -5,6 +5,7 @@ extern long g_registers[MAX_REGISTERS];
 extern char *s_registers[MAX_REGISTERS];
 extern char s_buff[GLOBAL_BUFF_SIZE];
 extern long *e_sp, *e_bp;
-extern char **j_sp, **j_bp;
+extern operation **j_sp, **j_bp;
+extern operation *pp, *pe;
 
 #endif //SIMPSCRIPT_GLOBALS_H
