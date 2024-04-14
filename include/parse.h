70ab3f9972f00fe0473f728c6e3967b19e99ea4c
Caleb Stein <caleb@calebstein.net>
2024-04-13 18:47:06 -0700

    don't expose parsing functions globally

--- include/parse.h
+++ include/parse.h
@@ -1,10 +1,7 @@
 #ifndef SIMPLANG_PARSE_H
 #define SIMPLANG_PARSE_H
 
-void parse_op(operation *op);
 void get_opcode(operation *op, char *tok);
-void parse_one_arg(operation *op);
-void parse_two_args(operation *op);
-void parse_three_args(operation *op);
+void parse_op(operation *op);
 
 #endif //SIMPLANG_PARSE_H
