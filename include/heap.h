cc3b4850627e3d26a33e57ef9240c434a931544f
Caleb Stein <caleb@calebstein.net>
2024-04-17 12:17:00 -0700

    slightly simplify heap code

--- include/heap.h
+++ include/heap.h
@@ -1,7 +1,7 @@
 #ifndef SIMPLANG_HEAP_H
 #define SIMPLANG_HEAP_H
 
-#define HEADER_SIG 0x53504850
+#define HEADER_SIG 0x50485053
 
 typedef struct {
     int sig;
@@ -9,14 +9,10 @@ typedef struct {
     ptr_type_e type;
 } heap_hdr_t;
 
-typedef union {
-    char *str_ptr;
-    long *int_ptr;
-    heap_hdr_t *hdr_ptr;
-} heap_ptr_u;
-
 void init_heap();
 void *simp_alloc(int size, ptr_type_e type);
 void simp_free(char *ptr);
+
+extern heap_hdr_t g_heap[GLOBAL_HEAP_SIZE];
 
 #endif //SIMPLANG_HEAP_H
--- src/heap.c
+++ src/heap.c
@@ -4,36 +4,36 @@
 #include "defs.h"
 #include "heap.h"
 
-char g_heap[GLOBAL_HEAP_SIZE] = {};
-heap_ptr_u heap_ptr = { .str_ptr = g_heap };
+heap_hdr_t g_heap[GLOBAL_HEAP_SIZE] = {};
+heap_hdr_t *heap_ptr = g_heap;
 int max_heap_blocks = GLOBAL_HEAP_SIZE / sizeof(heap_hdr_t);
 
 void init_heap() {
     heap_hdr_t hdr = { .sig = HEADER_SIG, .type = NONE, .size = max_heap_blocks };
-    *heap_ptr.hdr_ptr = hdr;
+    *heap_ptr = hdr;
 }
 
 void *simp_alloc(int size, ptr_type_e type) {
     int num_blocks = (size / sizeof(heap_hdr_t)) + 1;
     int offset_blocks = 0;
-    heap_ptr.str_ptr = g_heap;
-    while (heap_ptr.hdr_ptr->size < num_blocks || heap_ptr.hdr_ptr->type) {
+    heap_ptr = g_heap;
+    while (heap_ptr->size < num_blocks || heap_ptr->type) {
         if (offset_blocks + num_blocks >= max_heap_blocks) {
             printf("Heap full\n");
             exit(-1);
         }
-        offset_blocks += heap_ptr.hdr_ptr->size;
-        heap_ptr.hdr_ptr += (heap_ptr.hdr_ptr->size + 1);
+        offset_blocks += heap_ptr->size;
+        heap_ptr += (heap_ptr->size + 1);
     }
-    heap_ptr.hdr_ptr->size = num_blocks;
-    heap_ptr.hdr_ptr->type = type;
-    void *alloc_ptr = heap_ptr.hdr_ptr + 1;
-    heap_ptr.hdr_ptr += (heap_ptr.hdr_ptr->size + 1);
-    if (heap_ptr.hdr_ptr->sig != HEADER_SIG) {
+    heap_ptr->size = num_blocks;
+    heap_ptr->type = type;
+    void *alloc_ptr = heap_ptr + 1;
+    heap_ptr += (heap_ptr->size + 1);
+    if (heap_ptr->sig != HEADER_SIG) {
         heap_hdr_t hdr = { .sig = HEADER_SIG, .type = NONE };
-        if (!*(heap_ptr.str_ptr + sizeof(hdr) + 1)) {
-            hdr.size = max_heap_blocks - ((heap_ptr.str_ptr - g_heap) / sizeof(heap_hdr_t));
-            *heap_ptr.hdr_ptr = hdr;
+        if (!*(char *)((char *)(heap_ptr + 1) + 1)) {
+            hdr.size = max_heap_blocks - ((heap_ptr - g_heap) / sizeof(heap_hdr_t));
+            *heap_ptr = hdr;
         }
     }
 
@@ -43,18 +43,18 @@ void *simp_alloc(int size, ptr_type_e type) {
 void simp_free(char *ptr) {
     if (pe) return;
 
-    heap_ptr.str_ptr = ptr - sizeof(heap_hdr_t);
-    if (heap_ptr.hdr_ptr->sig != HEADER_SIG) return;
+    heap_ptr = (heap_hdr_t *)ptr - 1;
+    if (heap_ptr->sig != HEADER_SIG) return;
 
-    heap_ptr.hdr_ptr->type = NONE;
+    heap_ptr->type = NONE;
     int extra_blocks = 0;
-    heap_hdr_t *lookahead = heap_ptr.hdr_ptr;
+    heap_hdr_t *lookahead = heap_ptr;
     do {
-        lookahead += (heap_ptr.hdr_ptr->size + 1);
+        lookahead += (heap_ptr->size + 1);
         if (!lookahead->type) {
             extra_blocks += (lookahead->size + 1);
         }
     } while (!lookahead->type);
 
-    heap_ptr.hdr_ptr->size += extra_blocks;
+    heap_ptr->size += extra_blocks;
 }
