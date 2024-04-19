79ff2040aa80fe1ddff3be61687250792b5633ca
Caleb Stein <caleb@calebstein.net>
2024-04-19 13:16:53 -0700

    properly clear op struct on each instruction

--- src/repl.c
+++ src/repl.c
@@ -6,15 +6,15 @@
 #include "repl.h"
 
 void launch_repl() {
-    operation op = {};
-
     printf("Welcome to Simplang! Enter your commands at the prompt (done to exit):\n");
 
-    while(op.opcode != DONE) {
+    while(1) {
+        operation op = {};
         printf(">> ");
         fgets(s_buff, GLOBAL_BUFF_SIZE, stdin);
         if (s_buff[0] == '\n') continue;
         get_opcode(&op, s_buff);
+        if (op.opcode == DONE) break;
         if (!op.opcode || op.opcode == CMNT) continue;
         if (op.opcode == BEGLP || (ENDLPEQ <= op.opcode && op.opcode <= ENDLPGE)) {
             printf("Looping not yet supported in repl mode\n");
