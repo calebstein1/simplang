b791d51e08be975ca99e6ea7e49fb60b498fa5ab
Caleb Stein <caleb@calebstein.net>
2024-06-07 07:21:18 -0700

    add malloc.h

---
+++ include/malloc.h
@@ -0,0 +1,10 @@
+#ifndef MALLOC_H
+#define MALLOC_H
+
+#include <stdlib.h>
+
+#define malloc_or_fail(v, n, s) if (!(v = malloc(n * s))) { perror("malloc"); exit(-1); }
+#define calloc_or_fail(v, n, s) if (!(v = calloc(n, s))) { perror("calloc"); exit(-1); }
+#define realloc_or_fail(v, n, s) if (!(v = realloc(n, s))) { perror("realloc"); exit(-1); }
+
+#endif
