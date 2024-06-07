6fe0a744978c2bd5f2637558ddaf984184f15f55
Caleb Stein <caleb@calebstein.net>
2024-06-07 07:55:10 -0700

    fix malloc.h

--- include/malloc.h
+++ include/malloc.h
@@ -5,6 +5,6 @@
 
 #define malloc_or_fail(v, n, s) if (!(v = malloc(n * s))) { perror("malloc"); exit(-1); }
 #define calloc_or_fail(v, n, s) if (!(v = calloc(n, s))) { perror("calloc"); exit(-1); }
-#define realloc_or_fail(v, n, s) if (!(v = realloc(n, s))) { perror("realloc"); exit(-1); }
+#define realloc_or_fail(v, n, s) if (!(v = realloc(v, n * s))) { perror("realloc"); exit(-1); }
 
 #endif
